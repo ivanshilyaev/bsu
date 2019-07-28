444e74ae3844c75f521cf8f4e5918dd0506ae585
Ivan Shilyaev <shilyaev1433@gmail.com>
2019-07-28 15:31:56 +0300

    1st lab done

---
+++ educational_practice/c#/lab1/task1-3.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace laba_1
+{
+    class Program
+    {
+        // radiuses for task 2
+        static double r1=10, r2=5;
+        static void Main(string[] args)
+        {
+            // task 1; variant 14
+            double beginX=-4, endX=12, d=1;
+            Console.WriteLine("TASK 1:");
+            Console.WriteLine("Function value table for function #14:");
+            Console.WriteLine("{0,10} {1,10}", "x", "f(x)");
+            for (double i=beginX; i<=endX; i+=d) {
+                Console.WriteLine("{0,10} {1,10}", i, f(i));   // formatted output
+            }
+            Console.WriteLine();
+
+            // task 2, variant 14
+            Console.WriteLine("TASK 2:");
+            double x, y; string buf;
+            Console.WriteLine("Enter ten points (x,y):");
+            for (int i=0; i<3; ++i) {
+                Console.Write("x: "); buf = Console.ReadLine(); x = Convert.ToDouble(buf);
+                Console.Write("y: "); buf = Console.ReadLine(); y = Convert.ToDouble(buf);
+                if (isHitted(x, y)) {Console.WriteLine("Hit!");}
+                else {Console.WriteLine("Miss!");}
+            }
+            Console.WriteLine();
+
+            // task 3, variant 14
+            Console.WriteLine("TASK 3:");
+            Console.WriteLine("Taylor series for cos function:");
+            double eps = 0.0001;
+            double a=0, b=0; getBoundaries(ref a, ref b);
+            d = System.Math.Abs(b-a)/5;
+            Console.WriteLine("{0} {1} {2}", "x", "cos(x)", "n");
+            for (double i=a; i<=b; i+=d) {
+                int n=0;
+                Console.WriteLine("{0,7}   {1:0.000,7}   {2,7}", i, fTaylor(i, eps, ref n), n);
+            }
+        }
+
+        // function for task 1
+        static double f(double x) {
+            if (x>=-4 && x<=0) {return x/4;}
+            else if (x>=0 && x<=2) {return x*x;}
+            else if (x>=2 && x<=12) {return -0.5 * x + 5;}
+            return 0;
+        }
+
+        // function for task 2
+        static bool isHitted(double x, double y) {
+            if ( ((x>=0 && y>=0) || (x<=0 && y<=0)) && (x*x + y*y)>=r2*r2 && (x*x + y*y)<=r1*r1)
+                return true;
+            return false;
+        }
+
+        // functions for task 3
+        // static int factorial(int n) {
+        //     if (n<=0) {return 1;}
+        //     else {
+        //         int result=1;
+        //         for (int i=1; i<=n; ++i) {result*=i;}
+        //         return result;
+        //     }
+        // }
+
+        // static double func(double x, int n) {
+        //     return System.Math.Pow(-1, n)*System.Math.Pow(x, 2*n)/factorial(2*n);
+        // }
+        
+        static double fTaylor(double x, double eps, ref int n) {
+            double sum=1, s1=1, s2=0; n=1;
+            do {
+                s2=s1;
+                s1*=(-1)*Math.Pow(x, 2)/((2*n-1)*(2*n));
+                n++;
+                sum+=s1;
+            } while (System.Math.Abs(s1 - s2) > eps);
+            return sum;
+        }
+
+        static void getBoundaries(ref double a, ref double b) {
+            string buf;
+            Console.WriteLine("Enter left boundary:");
+            buf = Console.ReadLine(); a = Convert.ToDouble(buf);
+            Console.WriteLine("Enter right boundary:");
+            buf = Console.ReadLine(); b = Convert.ToDouble(buf);
+            while (b<=a) {
+                Console.WriteLine("Invalid data!");
+                Console.WriteLine("Enter right boundary:");
+                buf = Console.ReadLine(); b = Convert.ToDouble(buf);
+            }
+        }
+    }
+}
---
+++ educational_practice/c#/lab1/task4.cs
@@ -0,0 +1,203 @@
+﻿using System;
+
+namespace laba_1__class_
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plane[] planes = { new Plane("London", 654321, new Time(0, 4)),
+                               new Plane("Los Angeles", 413265, new Time(19, 30)),
+                               new Plane("New York", 987654, new Time(23, 50)) };
+            Airport airport = new Airport(planes);
+            airport.sort();
+            Console.WriteLine("1. Plane by index:");
+            airport.printPlanesByNumber(413265);
+            Console.WriteLine("2. Plane by time:");
+            airport.printPlanesByTime(new Time(23, 5));
+            Console.WriteLine("3. Plane by time:");
+            airport.printPlanesByDestination("New York");
+            Console.WriteLine("4. Compare two planes by time:");
+            Console.WriteLine(planes[0].getTime()<planes[1].getTime());
+        }
+    }
+
+
+
+    class Time {
+        private int hours;
+        private int minutes;
+
+        public int getHours() {
+            return this.hours;
+        }
+
+        public void setHours(int hours) {
+            this.hours = hours;
+        }
+
+        public int getMinutes() {
+            return this.minutes;
+        }
+
+        public void setMinutes(int minutes) {
+            this.minutes = minutes;
+        }
+
+        public Time() {hours=0; minutes=0;}
+        public Time(int hours, int minutes) {this.hours=hours%24; this.minutes=minutes%60;}
+
+        public int getTimeInMinutes() {
+            return hours * 60 + minutes;
+        }
+
+        public static bool operator < (Time t1, Time t2) {
+            return t1.getTimeInMinutes() < t2.getTimeInMinutes();
+        }
+
+        public static bool operator > (Time t1, Time t2) {
+            return t1.getTimeInMinutes() > t2.getTimeInMinutes();
+        }
+
+        public static bool operator <=(Time t1, Time t2) {
+            return t1.getTimeInMinutes() <= t2.getTimeInMinutes();
+        }
+
+        public static bool operator >= (Time t1, Time t2) {
+            return t1.getTimeInMinutes() >= t2.getTimeInMinutes();
+        }
+    }
+
+
+
+    class Plane {
+        private string destination;
+        private int number;
+        private Time time;
+
+        public Time getTime() {
+            return this.time;
+        }
+
+        public void setTime(Time time) {
+            this.time = time;
+        }
+
+
+        public string getDestination() {
+            return this.destination;
+        }
+
+        public void setDestination(string destination) {
+            this.destination = destination;
+        }
+
+        public int getNumber() {
+            return this.number;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public Plane() {
+            destination="Minsk"; number=123456; time=new Time(0, 0);
+        }
+        public Plane(string destination, int number, Time time) {
+            this.destination=destination;
+            this.number=number;
+            this.time=time;
+        }
+
+        public void Print() {
+            Console.WriteLine(destination);
+            Console.WriteLine(number);
+            if (time.getMinutes() < 10) {
+                Console.WriteLine("{0}:0{1}", time.getHours(), time.getMinutes());
+            }
+            else {
+                Console.WriteLine("{0}:{1}", time.getHours(), time.getMinutes());
+            }
+        }
+
+        public static bool operator < (Plane p1, Plane p2) {
+            return p1.getTime() < p2.getTime();
+        }
+
+        public static bool operator > (Plane p1, Plane p2) {
+            return p1.getTime() > p2.getTime();
+        }
+    }
+
+
+
+    class Airport {
+        private Plane[] planes;
+
+        public Plane[] getPlanes() {
+            return this.planes;
+        }
+
+        public void setPlanes(Plane[] planes) {
+            this.planes = planes;
+        }
+
+
+        public Airport(Plane[] planes) {
+            this.planes=planes;
+        }
+
+        public void sort() {
+            for (int i=0; i<planes.Length-1; ++i) {
+                for (int j=i+1; j<planes.Length; ++j) {
+                    if (planes[i]>planes[j]) {
+                        Plane tmp = planes[i];
+                        planes[i] = planes[j];
+                        planes[j] = tmp;
+                    }
+                }
+            }
+        }
+
+        public void printPlanesByNumber(int number) {
+            bool found = false;
+            for (int i=0; i<planes.Length; ++i) {
+                if (planes[i].getNumber() == number) {
+                    planes[i].Print();
+                    found = true;
+                }
+            }
+            if (!found) {Console.WriteLine("No such planes!");}
+        }
+
+        public void printPlanesByTime(Time time) {
+            for (int i=0; i<planes.Length; ++i) {
+                if (time.getHours()<23 || planes[i].getTime().getHours() == 23) {
+                    if (planes[i].getTime().getTimeInMinutes() - time.getTimeInMinutes() <= 60)
+                        planes[i].Print();
+                }
+                else if (planes[i].getTime().getHours() == 0) {
+                    // 1440 minutes in 24  hours
+                    if (planes[i].getTime().getTimeInMinutes() + 1440 - time.getTimeInMinutes() >= 0)
+                        planes[i].Print();
+                }
+            }
+        }
+
+        public void printPlanesByDestination(string dest) {
+            bool found = false;
+            for (int i=0; i<planes.Length; ++i) {
+                if (planes[i].getDestination().Equals(dest)) {
+                    planes[i].Print();
+                    found = true;
+                }
+            }
+            if (!found) {Console.WriteLine("No such planes!");}
+        }
+
+        public void Print() {
+            for (int i=0; i<planes.Length; ++i) {planes[i].Print();}
+        }
+    }
+}
