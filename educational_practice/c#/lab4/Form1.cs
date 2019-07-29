727118ac5b8cc50971f23473156cab85ee06b9ee
Ivan Shilyaev <shilyaev1433@gmail.com>
2019-07-29 13:04:41 +0300

    lab4 done

---
+++ educational_practice/c#/lab4/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba_4
+{
+    public partial class Form1 : Form
+    {
+
+        Сuckoo cuckoo = new Сuckoo("Cuckoo");
+        Chicken chicken = new Chicken("Chicken");
+        Hen hen = new Hen("Hen");
+        Cock cock = new Cock("Cock");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cuckoo.sing();
+            pictureBox1.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chicken.sing();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            hen.sing();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cock.sing();
+        }
+    }
+}
---
+++ educational_practice/c#/lab4/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba_4
+{
+    abstract class Bird
+    {
+        public string name;
+        public SoundPlayer player;
+        public Bird(string name)
+        {
+            this.name = name;
+        }
+        public void sing()
+        {
+            player.Play();
+        }
+    }
+
+    class Сuckoo : Bird // кукушка
+    {
+        public Сuckoo(string name) : base(name)
+        {
+            player = new System.Media.SoundPlayer("Cuckoo.wav"); 
+        }
+
+        public void fly()
+        {
+
+        }
+    }
+
+    class Chicken : Bird // курица
+    {
+        public Chicken(string name) : base(name)
+        {
+            player = new System.Media.SoundPlayer("Chicken.wav");
+        }
+    }
+
+    class Hen : Chicken // наседка
+    {
+        public Hen(string name) : base(name)
+        {
+            player = new System.Media.SoundPlayer("Hen.wav");
+        }
+    }
+
+    class Cock : Bird
+    {
+        public Cock(string name) : base(name)
+        {
+            player = new System.Media.SoundPlayer("Cock.wav");
+        }
+
+    }
+
+
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
