c8ed3da5e21e4f39e2d4f4ea36e1397628f5501a
Ivan Shilyaev <shilyaev1433@gmail.com>
2019-09-16 16:10:45 +0300

    lab2 completed

---
+++ programming/java/lab2/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String temp = line.replaceAll("\\D", "/");
+            StringTokenizer token = new StringTokenizer(temp, "/");
+            while (token.hasMoreTokens()) {
+                String elem = token.nextToken();
+                if (elem.length() > 2) {
+                    if (line.indexOf(elem) > 0 && line.charAt(line.indexOf(elem)-1) == '.') {
+                        line = line.replaceFirst(elem, elem.substring(0, 2));
+                    }
+                }
+            }
+            System.out.println(line);
+        }
+        sc.close();
+    }
+}
