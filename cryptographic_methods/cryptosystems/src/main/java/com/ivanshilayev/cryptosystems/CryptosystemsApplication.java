aa7c1e7f083f68f65b4f61fce9a0579a969d2154
Ivan Shilyaev <shilyaev1433@gmail.com>
2020-09-23 11:36:45 +0300

    Create web application

---
+++ cryptographic_methods/cryptosystems/src/main/java/com/ivanshilayev/cryptosystems/CryptosystemsApplication.java
@@ -0,0 +1,13 @@
+package com.ivanshilayev.cryptosystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptosystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptosystemsApplication.class, args);
+	}
+
+}
