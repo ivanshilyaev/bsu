31e3518a2df8bcbd0edcc4c39fb8b5f2b1caaa6a
Ivan Shilyaev <shilyaev1433@gmail.com>
2020-09-23 19:00:55 +0300

    Add encryption and decryption by SimpleSubstitutionCipher

--- cryptographic_methods/cryptosystems/src/main/java/com/ivanshilayev/cryptosystems/Runner.java
+++ cryptographic_methods/cryptosystems/src/main/java/com/ivanshilayev/cryptosystems/Runner.java
@@ -5,15 +5,16 @@ import com.ivanshilayev.cryptosystems.service.Cipher;
 import com.ivanshilayev.cryptosystems.service.exception.CipherException;
 import com.ivanshilayev.cryptosystems.service.impl.AffineCipher;
 import com.ivanshilayev.cryptosystems.service.impl.CaesarCipher;
+import com.ivanshilayev.cryptosystems.service.impl.SimpleSubstitutionCipher;
 import com.ivanshilayev.cryptosystems.service.utils.AffineCipherUtils;
 
 public class Runner {
     public static void main(String[] args) {
         try {
             Alphabet alphabet = new Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-            Cipher cipher = new AffineCipher();
+            Cipher cipher = new SimpleSubstitutionCipher();
             String text = "ABCD";
-            String key = "DE";
+            String key = "XYZABCDEFGHIJKLMNOPQRSTUVW";
             String encrypted = cipher.encrypt(alphabet, text, key);
             String decrypted = cipher.decrypt(alphabet, encrypted, key);
             System.out.println(encrypted);
---
+++ cryptographic_methods/cryptosystems/src/main/java/com/ivanshilayev/cryptosystems/service/impl/SimpleSubstitutionCipher.java
@@ -0,0 +1,32 @@
+package com.ivanshilayev.cryptosystems.service.impl;
+
+import com.ivanshilayev.cryptosystems.model.Alphabet;
+import com.ivanshilayev.cryptosystems.service.Cipher;
+import com.ivanshilayev.cryptosystems.service.exception.CipherException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SimpleSubstitutionCipher implements Cipher {
+    @Override
+    public String encrypt(Alphabet alphabet, String text, String key) throws CipherException {
+        List<Character> substitution = key.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+        StringBuilder encryptedText = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            int index = (alphabet.getSymbols().indexOf(c));
+            encryptedText.append(substitution.get(index));
+        }
+        return encryptedText.toString();
+    }
+
+    @Override
+    public String decrypt(Alphabet alphabet, String text, String key) throws CipherException {
+        List<Character> substitution = key.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+        StringBuilder decryptedText = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            int index = (substitution.indexOf(c));
+            decryptedText.append(alphabet.getSymbols().get(index));
+        }
+        return decryptedText.toString();
+    }
+}
