eb98a0423f9f8e6587c95e034ee081ab784643b9
Ivan Shilyaev <shilyaev1433@gmail.com>
2020-10-04 13:59:23 +0300

    Use relative pass. Add error handlers

--- cryptographic_methods/cryptosystems/src/main/java/com/ivanshilyaev/cryptosystems/ConsoleAppRunner.java
+++ cryptographic_methods/cryptosystems/src/main/java/com/ivanshilyaev/cryptosystems/ConsoleAppRunner.java
@@ -11,47 +11,49 @@ import com.ivanshilyaev.cryptosystems.validator.Validator;
 import com.ivanshilyaev.cryptosystems.validator.exception.ValidationException;
 import com.ivanshilyaev.cryptosystems.validator.impl.*;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ConsoleAppRunner {
 
-    private static final String ALPHABET_PATH_DEFAULT = "/Users/ivansilaev/Downloads/gitRepos/bsu/cryptographic_methods/cryptosystems/src/main/resources/alphabet.txt";
-    private static final String TEXT_PATH_DEFAULT = "/Users/ivansilaev/Downloads/gitRepos/bsu/cryptographic_methods/cryptosystems/src/main/resources/in.txt";
-    private static final String KEY_PATH_DEFAULT = "/Users/ivansilaev/Downloads/gitRepos/bsu/cryptographic_methods/cryptosystems/src/main/resources/key.txt";
-    private static final String ENCRYPT_PATH_DEFAULT = "/Users/ivansilaev/Downloads/gitRepos/bsu/cryptographic_methods/cryptosystems/src/main/resources/encrypt.txt";
-    private static final String DECRYPT_PATH_DEFAULT = "/Users/ivansilaev/Downloads/gitRepos/bsu/cryptographic_methods/cryptosystems/src/main/resources/decrypt.txt";
+    private static final String ALPHABET_PATH_DEFAULT = "src/main/resources/alphabet.txt";
+    private static final String TEXT_PATH_DEFAULT = "src/main/resources/in.txt";
+    private static final String KEY_PATH_DEFAULT = "src/main/resources/key.txt";
+    private static final String ENCRYPT_PATH_DEFAULT = "src/main/resources/encrypt.txt";
+    private static final String DECRYPT_PATH_DEFAULT = "src/main/resources/decrypt.txt";
     private static final String DEFAULT = "d";
 
     private void start() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter alphabet file path (d - default, 0 - default alphabet):");
-        String alphabetPath = scanner.nextLine();
-        if (alphabetPath.equals(DEFAULT)) {
-            alphabetPath = ALPHABET_PATH_DEFAULT;
-        }
-        System.out.println("Enter text file path (d - default):");
-        String textPath = scanner.nextLine();
-        if (textPath.equals(DEFAULT)) {
-            textPath = TEXT_PATH_DEFAULT;
-        }
-        System.out.println("Enter key file path (d - default):");
-        String keyPath = scanner.nextLine();
-        if (keyPath.equals(DEFAULT)) {
-            keyPath = KEY_PATH_DEFAULT;
-        }
-        System.out.println("Enter operation: 0 - encrypt, 1 - decrypt");
-        int operationChoice = scanner.nextInt();
-        System.out.println("Enter cryptosystem to use:");
-        System.out.println("0 - Caesar cipher");
-        System.out.println("1 - Affine cipher");
-        System.out.println("2 - Simple substitution cipher");
-        System.out.println("3 - Hill cipher");
-        System.out.println("4 - Transposition cipher");
-        System.out.println("5 - Vigenere cipher");
-        int cipherChoice = scanner.nextInt();
-
-        LineReaderCommand readerCommand = new LineReaderCommand();
         try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter alphabet file path (d - default, 0 - default alphabet):");
+            String alphabetPath = scanner.nextLine();
+            if (alphabetPath.equals(DEFAULT)) {
+                alphabetPath = ALPHABET_PATH_DEFAULT;
+            }
+            System.out.println("Enter text file path (d - default):");
+            String textPath = scanner.nextLine();
+            if (textPath.equals(DEFAULT)) {
+                textPath = TEXT_PATH_DEFAULT;
+            }
+            System.out.println("Enter key file path (d - default):");
+            String keyPath = scanner.nextLine();
+            if (keyPath.equals(DEFAULT)) {
+                keyPath = KEY_PATH_DEFAULT;
+            }
+            System.out.println("Enter operation: 0 - encrypt, 1 - decrypt");
+            int operationChoice = scanner.nextInt();
+            System.out.println("Enter cryptosystem to use:");
+            System.out.println("0 - Caesar cipher");
+            System.out.println("1 - Affine cipher");
+            System.out.println("2 - Simple substitution cipher");
+            System.out.println("3 - Hill cipher");
+            System.out.println("4 - Transposition cipher");
+            System.out.println("5 - Vigenere cipher");
+            int cipherChoice = scanner.nextInt();
+
+            LineReaderCommand readerCommand = new LineReaderCommand();
+
             String alphabetString;
             if (alphabetPath.equals("0")) {
                 alphabetString = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ";
@@ -70,45 +72,38 @@ public class ConsoleAppRunner {
             Cipher cipher;
 
             switch (cipherChoice) {
-                case 0: {
+                case 0 -> {
                     cipher = new CaesarCipher();
                     validator = new CaesarCipherKeyValidator(alphabet);
                     validator.validate(key);
-                    break;
                 }
-                case 1: {
+                case 1 -> {
                     cipher = new AffineCipher();
                     validator = new AffineCipheKeyValidator(alphabet);
                     validator.validate(key);
-                    break;
                 }
-                case 2: {
+                case 2 -> {
                     cipher = new SimpleSubstitutionCipher();
                     validator = new SimpleSubstitutionCipherKeyValidator(alphabet);
                     validator.validate(key);
-                    break;
                 }
-                case 3: {
+                case 3 -> {
                     cipher = new HillCipher();
                     validator = new HillCipherKeyValidator(alphabet);
                     validator.validate(key);
-                    break;
                 }
-                case 4: {
+                case 4 -> {
                     cipher = new TranspositionCipher();
                     validator = new TranspositionCipherKeyValidator(alphabet);
                     validator.validate(key);
-                    break;
                 }
-                case 5: {
+                case 5 -> {
                     cipher = new VigenereCipher();
                     validator = new VigenereCipherKeyValidator(alphabet);
                     validator.validate(key);
-                    break;
                 }
-                default: {
+                default -> {
                     cipher = new CaesarCipher();
-                    break;
                 }
             }
 
@@ -135,6 +130,8 @@ public class ConsoleAppRunner {
             System.out.println(e.getMessage());
         } catch (CipherException e) {
             e.printStackTrace();
+        } catch (InputMismatchException e) {
+            System.out.println("Error: incorrect input!");
         }
     }
 
